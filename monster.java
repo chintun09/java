98eff8f20b3ab3c56e6ec220d081999610935ba9
chintun09 <164408751+chintun09@users.noreply.github.com>
2024-03-21 19:55:25 +0000

    face

---
+++ monster.java
@@ -0,0 +1,12 @@
+public class monster {
+    
+        public static void main(String[] args) {
+            System.out.println("  11111");
+            System.out.println(" 1     1");
+            System.out.println("1  o o 1");
+            System.out.println("1   -   1");
+            System.out.println(" 1 \\___/ 1");
+            System.out.println("  1_____1");
+        }
+    }
+
